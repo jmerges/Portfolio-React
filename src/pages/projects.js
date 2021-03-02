e9ee991ce45bad84845221704ff5c8c1098d58a2
jmerges bootcamp <jamesmerges1@gmail.com>
2021-03-01 23:54:14 -0800

    WorkoutTracker finished

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,12 +4,14 @@ import CodingHomepage from "../components/CodingHomepage";
 import ClickyGame from "../components/ClickyGame";
 import WeatherDashboard from "../components/WeatherDashboard";
 import CodeQuiz from "../components/CodeQuiz";
+import WorkoutTracker from "../components/WorkoutTracker";
 
 function Projects () {
     return (
     <div>
         <CountyElection />
         <CodingHomepage />
+        <WorkoutTracker />
         <ClickyGame />
         <WeatherDashboard />
         <CodeQuiz />
